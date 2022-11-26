cfdb76f6508a05fc0f4d1ceb3f5e99ed77a68e04
Bogdan Bog <nedbog@gmail.com>
2022-11-26 11:52:37 +0200

    iterate through employees

--- BethanysPieShopHRM.Blazor/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM.Blazor/Pages/EmployeeOverview.razor.cs
@@ -1,6 +1,15 @@
-﻿namespace BethanysPieShopHRM.Blazor.Pages
+﻿using BethanysPieShopHRM.App.Models;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanysPieShopHRM.Blazor.Pages
 {
     public partial class EmployeeOverview
     {
+        public List<Employee>? Employees { get; set; } = default;
+
+        protected override void OnInitialized()
+        {
+            Employees = MockDataService.Employees;
+        }
     }
 }
