e43cea6a15dcc47d8df47483a9a0ccf41119f4e9
nedbog <bogdan.nedelcu@ifacts.se>
2023-01-08 15:39:04 +0200

    employee detail page component

--- BethanysPieShopHRM.Blazor/Components/EmployeeCard.razor.cs
+++ BethanysPieShopHRM.Blazor/Components/EmployeeCard.razor.cs
@@ -9,5 +9,13 @@ namespace BethanysPieShopHRM.Blazor.Components
         public Employee Employee { get; set; } = default!;
         [Parameter]
         public EventCallback<Employee> EmployeeQuickViewClicked { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = default!;
+
+        public void NavigateToDetails(Employee selectedEmployee)
+        {
+            NavigationManager.NavigateTo($"/employeedetail/{selectedEmployee.EmployeeId}");
+        }
     }
 }
