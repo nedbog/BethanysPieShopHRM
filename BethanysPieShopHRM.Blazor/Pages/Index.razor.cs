db8d681cfdef2a8e13662887ed74b29327b0c3cd
Bogdan Bog <nedbog@gmail.com>
2023-02-03 22:05:02 +0200

    dynamic component

---
+++ BethanysPieShopHRM.Blazor/Pages/Index.razor.cs
@@ -0,0 +1,12 @@
+﻿using BethanysPieShopHRM.Blazor.Components.Widgets;
+
+namespace BethanysPieShopHRM.Blazor.Pages
+{
+    public partial class Index
+    {
+        public List<Type> Widgets { get; set; } = new List<Type>
+        {
+            typeof(EmployeeCountWidget), typeof(InboxWidget)
+        };
+    }
+}
