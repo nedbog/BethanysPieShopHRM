3c296ca3ea99367491ed053b4b725caab81c9493
Bogdan Bog <nedbog@gmail.com>
2023-02-03 22:18:30 +0200

    page title builtin component

--- BethanysPieShopHRM.Blazor/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM.Blazor/Pages/EmployeeOverview.razor.cs
@@ -7,6 +7,7 @@ namespace BethanysPieShopHRM.Blazor.Pages
     {
         public List<Employee>? Employees { get; set; } = default;
         private Employee? _selectedEmployee;
+        private string Title = "Employee overview";
 
         protected override void OnInitialized()
         {
